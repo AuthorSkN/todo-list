5097c27358e3b107600dcaf2124b98e095c67000
AuthorSkN <author.skn@gmail.com>
2022-05-01 23:21:20 +0400

    create simple react-app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  const searchText = "Type here to search";
+  const searchStyle = {
+    fontSize: "20px",
+  };
+
+  return <input style={searchStyle} placeholder={searchText} />;
+};
+
+const TodoList = () => {
+  const items = ["Learn React", "Build Awesome App"];
+
+  return (
+    <ul>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+    </ul>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
